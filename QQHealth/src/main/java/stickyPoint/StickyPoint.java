ca7428dc09785b9c1d56948963fedc1f29155e50
nbwzlyd <420907013@qq.com>
2016-08-02 17:17:47 +0800

    增加activity基类

--- QQHealth/src/main/java/stickyPoint/StickyPoint.java
+++ QQHealth/src/main/java/stickyPoint/StickyPoint.java
@@ -3,6 +3,8 @@ package stickyPoint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -13,6 +15,8 @@ import android.view.View;
 public class StickyPoint extends View {
 
     private static final int MAX_PULL = 15;//最大的下拉距离,超过该距离则直接
+    private VPoint mVpoint = new VPoint();
+    private HPoint mHpoint = new HPoint();
 
 
     private Paint mPointPaint;
@@ -34,5 +38,25 @@ public class StickyPoint extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+    }
+
+    class VPoint {
+
+        private int x;
+        private int y;
+        private RectF topRect;
+        private RectF bottomRect;
+
+        public void setX(int x){
+            this.x = x;
+
+
+        }
+
+
+    }
+
+    class HPoint {
+
     }
 }
